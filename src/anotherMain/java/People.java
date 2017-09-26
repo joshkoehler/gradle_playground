c09176fac815990c7ad51d69fb4123daede455e1
Joshua Koehler <josh.s.koehler@gmail.com>
2017-09-25 23:47:51 -0400

    Messing around with gradle and java sourcesets

---
+++ src/anotherMain/java/People.java
@@ -0,0 +1,10 @@
+package com.another.joshkoehler;
+
+public class People {
+  public People(){
+  }
+
+  public String toString() {
+    return "You have created a people!";
+  } 
+}
---
+++ src/main/java/Hello.java
@@ -0,0 +1,11 @@
+package com.joshkoehler;
+
+import com.another.joshkoehler.People;
+
+public class Hello {
+  public static void main(String[] args) {
+    People people = new People();
+    System.out.println("Hello, World");
+    System.out.println(people);
+  }
+}
